0002d2f44785985321f10fff264b6c7dcd0ec29f
ishan27012011 <81416295+ishan27012011@users.noreply.github.com>
2022-10-27 19:32:34 +0530

    code v.1

---
+++ main.js
@@ -0,0 +1,30 @@
+status = "";
+objects = [];
+video = "";
+
+function preload() {
+    video = createVideo("video.mp4");
+}
+
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video.hide()
+}
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0.5);
+    console.log("Model Loaded")
+}
