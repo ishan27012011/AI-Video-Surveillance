fd63e26325e41e5ff4f91d5d48d6eacc542e6bbc
ishan27012011 <81416295+ishan27012011@users.noreply.github.com>
2022-10-28 18:53:04 +0530

    code v.2

--- main.js
+++ main.js
@@ -12,9 +12,33 @@ function setup() {
     video.hide()
 }
 
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+    }
+}
+
 function draw() {
     image(video, 0, 0, 480, 380);
+    if (status != "") {
+        objectDetector.detect(video, gotResult)
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("numberOfObjectsDetected").innerHTML = "Number of objects detected: " + objects.length;
+            document.getElementById("status").innerHTML = "Status: Detected Objects";
+            percentage = floor(objects[i].confidence * 100);
+            fill("red");
+            text(objects[i].label + " " + percentage + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
+
 
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
